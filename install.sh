400233063f0ca11a83456134ed09a092b4cac238
eggman79 <eggman79@criptext.com>
2020-03-14 21:15:45 +0000

    fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y curl neovim cmake python3 python3-dev g++ golang npm git tmux exuberant-ctags
+apt-get install -y curl neovim cmake python3 python3-dev g++ clang libclang-dev git tmux exuberant-ctags
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

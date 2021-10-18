76bae59281b5ef96df2ea6b3b472d77245248502
eggman79 <eggman79@protonmail.com>
2021-10-19 00:09:50 +0200

    fix

--- install.sh
+++ install.sh
@@ -2,11 +2,15 @@
 
 apt-get update -y
 apt-get upgrade -y
-apt-get install -y curl neovim cmake python3 python3-dev g++ clang libclang-dev git tmux exuberant-ctags
+apt-get install -y curl neovim cmake g++ clang libclang-dev git tmux ccls nodejs npm
+npm install yarn -g
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim/
 cp -rf init.vim ~/.config/nvim
+cp -rf coc-settings.json ~/.config/coc-settings.json
 nvim +PlugInstall +qall
+cd ~/.config/nvim/plugged/coc.nvim
+yarn i

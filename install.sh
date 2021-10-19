946e696d6d1de4cdf1a91034aa53063c6eeea466
eggman79 <eggman79@protonmail.com>
2021-10-19 09:09:24 +0200

    fix

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+curr_dir=`pwd`
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y curl neovim cmake g++ clang libclang-dev git tmux ccls nodejs npm
@@ -9,8 +10,9 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.config/nvim/
-cp -rf init.vim ~/.config/nvim
-cp -rf coc-settings.json ~/.config/coc-settings.json
+cp -rvf init.vim ~/.config/nvim
+cp -rvf coc-settings.json ~/.config/nvim/
 nvim +PlugInstall +qall
 cd ~/.config/nvim/plugged/coc.nvim
-yarn i
+yarn install
+cd "$curr_dir"
